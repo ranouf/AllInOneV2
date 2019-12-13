ee00d5ade9b9eb18f69274d8e509050ca8d38c5d
Cedric <arcedric@wanadoo.fr>
2019-12-13 12:12:48 -0500

    Added Angular Material & Covalent (#3)

---
+++ src/client/src/app/modules/covalent.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import {
+  CovalentMediaModule, CovalentCommonModule, CovalentExpansionPanelModule, CovalentChipsModule, CovalentDialogsModule,
+  CovalentFileModule, CovalentPagingModule, CovalentNotificationsModule, CovalentSearchModule, CovalentMenuModule,
+  CovalentLoadingModule, TdLoadingService, CovalentLayoutModule, TdDialogService, CovalentDataTableModule
+} from '@covalent/core';
+
+@NgModule({
+  exports: [
+    CovalentLayoutModule,
+    CovalentLoadingModule,
+    CovalentMenuModule,
+    CovalentSearchModule,
+    CovalentNotificationsModule,
+    CovalentPagingModule,
+    CovalentFileModule,
+    CovalentDialogsModule,
+    CovalentMediaModule,
+    CovalentCommonModule,
+    CovalentExpansionPanelModule,
+    CovalentChipsModule,
+    CovalentDataTableModule
+  ],
+  providers: [
+    TdDialogService,
+  ]
+})
+export class CovalentModule { }
